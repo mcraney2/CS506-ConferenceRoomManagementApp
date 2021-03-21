1caea9e0ca5cbef61232c8a04575bdcd41e559a9
Connor Craney <mcraney2@wisc.edu>
2021-03-21 17:50:36 -0500

    Completed my three screens

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -3,31 +3,54 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from "../components/Button.js";
+import TextBox from "../components/TextBox.js"
 
 export function LogInScreen({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Log-In</Text>
-            <Button 
-                handleClick={() =>
-                    navigation.navigate('ManagementConsole')
-                }
-                label="Log-In"
-            />
-            <Text>New User</Text>
-            <Button 
-                handleClick={() =>
-                    navigation.navigate('SignUpScreen')
-                }
-                label="Enter"
-            />
-            <Text>Kiosk Mode</Text>
-            <Button 
-                handleClick={() =>
-                    navigation.navigate('KioskSetUp')
-                }
-                label="Enter"
-            />
+            <View style={styles.LogInContainer}>
+                <Text style={styles.textSty}>Log-In</Text>
+                <TextBox 
+                    height={30}
+                    width={200}
+                    borderColor={"grey"}
+                    borderWidth={1}
+                    margin={10}
+                    placeholder="Username"
+                />
+                <TextBox 
+                    height={30}
+                    width={200}
+                    borderColor={"grey"}
+                    borderWidth={1}
+                    margin={10}
+                    placeholder="Password"
+                />
+                <Button 
+                    handleClick={() =>
+                        navigation.navigate('ManagementConsole')
+                    }
+                    label="Log-In"
+                />
+            </View>
+            <View style={styles.SecondaryContainer}>
+                <Text style={styles.textSty}>New Users</Text>
+                <Button 
+                    handleClick={() =>
+                        navigation.navigate('SignUpScreen')
+                    }
+                    label="Enter"
+                />
+            </View>
+            <View style={styles.SecondaryContainer}>
+                <Text style={styles.textSty}>Kiosk Mode</Text>
+                <Button 
+                    handleClick={() =>
+                        navigation.navigate('KioskSetUp')
+                    }
+                    label="Enter"
+                />
+            </View>
         </View>
     );
 }
@@ -38,6 +61,19 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    LogInContainer: {
+        flex:0.4,
+    },
+    SecondaryContainer: {
+        flex:0.3,
+    },
+    textSty: {
+        fontSize:30,
+        fontWeight:'bold',
+        margin: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
   });
   
