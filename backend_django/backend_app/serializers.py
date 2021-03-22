6197b7d34d2ee52bca376c3582e13c8641902020
vince <hzhu226@wisc.edu>
2021-03-22 12:55:43 -0500

    Finish serializer

--- backend_django/backend_app/serializers.py
+++ backend_django/backend_app/serializers.py
@@ -2,20 +2,29 @@ from rest_framework import serializers
 from backend_app.models import Admin, Group, Room, DailyCalendar, Event, Request
 from  django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
 class AdminSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
     class Meta:
         model = Admin
-        fields = '__all__' 
+        fields = ['user']
 
 class GroupSerializer(serializers.ModelSerializer):
+    managername = serializers.StringRelatedField(source='manager', read_only=True)
+    user = UserSerializer(read_only = True)
     class Meta:
         model = Group
-        fields = '__all__' 
+        fields = ['groupname', 'groupcode', 'managername','user'] 
 
 class RoomSerializer(serializers.ModelSerializer):
+    groups = serializers.StringRelatedField(source='group.groupname', read_only=True)
     class Meta:
         model = Room
-        fields = '__all__'  
+        fields = ['roomnumber', 'groups']  
 
 class DailyCalendarSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,11 +32,17 @@ class DailyCalendarSerializer(serializers.ModelSerializer):
         fields = '__all__'  
 
 class EventSerializer(serializers.ModelSerializer):
+    roomId = serializers.CharRelatedField(source='room.roomnumber', read_only=True)
+    admin = serializers.StringRelatedField(source='creator', read_only=True)
+    dates = serializers.DateRelatedField(source='date', read_only=True)
     class Meta:
         model = Event
         fields = '__all__'  
 
 class RequestSerializer(serializers.ModelSerializer):
+    requestors = serializers.ReadOnlyField(source='requestor')
+    roomId = serializers.CharRelatedField(source='room.roomnumber', read_only=True)
+    conflicts = EventSerializer(read_only = True)
     class Meta:
         model = Request
         fields = '__all__' 
