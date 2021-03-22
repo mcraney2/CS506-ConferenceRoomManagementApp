94ab06948407a3e78dd07ad37165e2e76caedd68
vince <hzhu226@wisc.edu>
2021-03-22 16:14:36 -0500

    Update views.py

--- backend_django/backend_app/views.py
+++ backend_django/backend_app/views.py
@@ -5,87 +5,130 @@ from rest_framework.parsers import JSONParser
 from backend_app.serializers import UserSerializer,AdminSerializer,GroupSerializer,RoomSerializer,DailyCalendarSerializer,EventSerializer,RequestSerializer
 from backend_app.models import Admin, Group, Room, DailyCalendar, Event, Request
 from  django.contrib.auth.models import User
-# from rest_framework import serializers
 from django.forms.models import model_to_dict
 from django.core import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+import json
+from rest_framework.decorators import api_view
 
 # login authentication
-# "login/"
+# "room_mgmt/login/"
+# request format: {"username": "ruisu","password":"zrs12345"}
+# response format: response = {"authenticated": False,"user": False,"new":False,}
 @api_view(['GET'])
 def login(request):
     if request.method == 'GET':
+        response = {
+            "authenticated": False,
+            "user": False,
+            "new": False,
+        }
         data = JSONParser().parse(request)
+        print(data)
         user = authenticate(username=data['username'], password=data['password'])
+        print(user)
         if user is not None:  # A backend authenticated the credentials
-            # TODO: report error to the frontend
+            response["authenticated"]=True       
             try:
                 admin = user.admin
-                # TODO: direct to admin's page
-                # TODO: check if admin is new (i.e., has creted a group)
+                try: 
+                    group = admin.group_set.objects.filter(id=1)
+                    return JsonResponse(response,status=201)
+                except:  # new admin
+                    response["new"]=True
+                    return JsonResponse(response,status=201)
             except:
-                # TODO: direct to user's page
-                # TODO: check if user is new (i.e., has joined in a group)
+                response["user"]=True
+                try: 
+                    group = user.group_set.objects.filter(id=1)
+                    return JsonResponse(response,status=201)
+                except:  # new user
+                    response["new"]=True
+                    return JsonResponse(response,status=201)
         else:  # No backend authenticated the credentials
-            # TODO: report error to the frontend
+            return JsonResponse(response,status=201) #TODO: better ways to send signin error reports
 
 # create new user account
-# "signup/"
+# "room_mgmt/signup/"
+# request format: {"username": "ruisu","password":"zrs12345", "user":"True"}
+# response format: {"created":False}
 @api_view(['POST'])
 def signup(request):
     if request.method == 'POST':
+        response = {"created":False}
         data = JSONParser().parse(request)  # TODO: is frontend/backend that checks the 2 parrwords match or not
-        user = User.objects.create_user(username=data['username'], password=data['password'])
-        #if:  # TODO: create a amin account
-            
+        try:
+            user = User.objects.get(username=data["username"])  # username already exists'
+            return JsonResponse(response, status=201)
+        except: # no user exists
+            user = User.objects.create_user(username=data['username'], password=data['password'])
+            if data["user"] is True:
+                response['created']=True
+                return JsonResponse(response, status=201)
+            else:
+                admin = Admin.objects.create(user=user)
+                admin.save()
+                response['created'] = True
+                return JsonResponse(response, status=201)
 
-# TODO: new admin creates a group
-@api_view(['POST'])
-def admin_create_group(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        # TODO: call serlizer
-        # TODO:
+# # TODO: new admin creates a group
+# @api_view(['POST'])
+# def admin_create_group(request):
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         # TODO: generate random group code
+#         # TODO: call serlizer
+#         # TODO:
         
 
-# TODO: admin adds a conference room
-@api_view(['POST'])
-def admin_add_confroom(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        # TODO: call serlizer
+# # TODO: admin adds a conference room
+# @api_view(['POST'])
+# def admin_add_confroom(request):
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         # TODO: call serlizer
 
 
-# TODO: admin goes to the request page
-@api_view(['GET'])
-def admin_view_requests(request):
-    if request.method == 'GET':
-        data = JSONParser().parse(request)
-        # TODO: get a list of requets, sorting by the request date and time
+# # TODO: admin goes to the request page
+# @api_view(['GET'])
+# def admin_view_requests(request):
+#     if request.method == 'GET':
+#         data = JSONParser().parse(request)
+#         # TODO: get a list of requets, sorting by the request date and time
         
 
-# TODO: admin approves/rejects a request
-@api_view(['PUT','POST'])
-def admin_process_request(request):
-    if request.method == 'PUT':  # admin rejects a request -> update request 
-        data = JSONParser().parse(request)
-    if request.method == 'PUT':  # admin aprrove a request -> update request and create corresponding events
-        data = JSONParser().parse(request)
+# # TODO: admin approves/rejects a request
+# @api_view(['PUT','POST'])
+# def admin_process_request(request):
+#     if request.method == 'PUT':  # admin rejects a request -> update request 
+#         data = JSONParser().parse(request)
+#     if request.method == 'PUT':  # admin aprrove a request -> update request and create corresponding events
+#         data = JSONParser().parse(request)
 
 # TODO: in edit event page, admin selects a date
 @api_view(['GET'])
 def admin_view_events(request):
     if request.method == 'GET':
         data = JSONParser().parse(request)
-        # TODO: get a list of events
+        # todo
+        return JsonResponse(serializer.data, safe=False)
+        
 
 # TODO: Event: admin click the “edit/delete” bottom
 @api_view(['PUT','DELETE'])
 def admin_manage_event(request):
     if request.method == 'PUT': # TODO: edit an event
         data = JSONParser().parse(request)
+        # todo
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
     if request.method == 'DELETE': # TODO: delte an event
+        ob = Event.objects.get(id=id)
+        ob.delete()
+        return JsonResponse(status=204)
        
 
 # TODO: admin creates events
@@ -93,7 +136,12 @@ def admin_manage_event(request):
 def admin_create_events(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        # TODO: call serlizer
+        admins = Admin.object.all()
+        serializer = EventSerializer(admins, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
 # TODO: new user joins a group'
@@ -101,13 +149,18 @@ def admin_create_events(request):
 def user_join_group(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        # TODO: call serlizer
+        users = User.object.all()
+        serializer = GroupSerializer(users, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
-# TODO: user requests calendar for a selected room ; user switches between daily/weekly display of the calendar; user moves to the next/last day/week
-@api_view(['GET'])
-def user_join_group(request):
-    if request.method == 'GET':
-        data = JSONParser().parse(request)
+# # TODO: user requests calendar for a selected room ; user switches between daily/weekly display of the calendar; user moves to the next/last day/week
+# @api_view(['GET'])
+# def user_join_group(request):
+#     if request.method == 'GET':
+#         data = JSONParser().parse(request)
 
 
 # TODO: user sends a request
@@ -115,9 +168,14 @@ def user_join_group(request):
 def user_send_request(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        # TODO: call serlizer
+        users = User.object.all()
+        serializer = RequestSerializer(users, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
-        
-# TODO: Kiosk
+
+# # TODO: Kiosk
 
 
