bc4575f98bc306b5a0429ce2685cb26047cd578b
Andrew Smart <asmart3@wisc.edu>
2021-03-25 20:42:25 -0700

    Added AddRooms Component

--- screens/UserConsole.js
+++ screens/UserConsole.js
@@ -45,15 +45,16 @@ export function UserConsole({navigation}) {
                         
                     />
           </View>
-          <View style={styles.calendarContainer}>
-            <Calendar events={events} height={400} />
-          </View>
-            <Button 
+          <Button 
                 handleClick={() =>
                     navigation.navigate('RequestRoomUser')
                 }
                 label="Request Conference Room"
             />
+          <View style={styles.calendarContainer}>
+            <Calendar events={events} height={400} />
+          </View>
+            
         </View>
     );
   }
