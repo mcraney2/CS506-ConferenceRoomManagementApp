d0a49a774f96e64d34fe4798800cb33670dd8162
zachchanak <47953007+zachchanak@users.noreply.github.com>
2021-03-26 17:19:45 -0500

    Delete backend_django directory
    yeet

--- backend_django/backend_django/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    path('room_mgmt/',include('backend_app.urls')),
-]
