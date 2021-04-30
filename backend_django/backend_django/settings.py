ebbc902d9565351032757f399ebb84d9cfef7df8
Connor Craney <mcraney2@wisc.edu>
2021-04-29 21:06:14 -0500

    Calendar times correct now

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Panama'
 
 USE_I18N = True
 
